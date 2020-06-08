b60ee5827834f90858d20806d8969f10bac03df0
CristhianChavez <chavezalonsoc@gmail.com>
2020-06-07 22:27:42 -0500

    Revert "primer commit"
    This reverts commit e996cde245169a08cd81194d983d1f4c548504e7.

--- operaciones3.js
+++
@@ -1,8 +0,0 @@
-var x; var y;
-x=8;
-y=10;
-document.getElementById('SUMA').innerHTML="El resultado de la suma es " + (x + y);
-document.getElementById('RESTA').innerHTML="El resultado de la resta es " + (x - y);
-document.getElementById('MULTIPLICACION').innerHTML="El resultado de la multiplicacion es " + (x * y);
-document.getElementById('DIVISON').innerHTML="El resultado de la division es " + (x / y);
-
